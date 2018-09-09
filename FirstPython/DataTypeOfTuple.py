80cf75c6f960601e29a7bea26630ef37f84e16ea
wangshiz <wangshiz@users.noreply.github.com>
2018-09-09 18:22:45 +0800

    abc

--- FirstPython/DataTypeOfTuple.py
+++ FirstPython/DataTypeOfTuple.py
@@ -11,3 +11,12 @@ print(tuple[1:3])  # 输出从第二个元素开始到第三个元素
 print(tuple[2:])  # 输出从第三个元素开始的所有元素
 print(tinytuple * 2)  # 输出两次元组
 print(tuple + tinytuple)  # 连接元组
+
+"""
+('abcd', 786, 2.23, 'runoob', 70.2)
+abcd
+(786, 2.23)
+(2.23, 'runoob', 70.2)
+(123, 'runoob', 123, 'runoob')
+('abcd', 786, 2.23, 'runoob', 70.2, 123, 'runoob')
+"""

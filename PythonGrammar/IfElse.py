da3fbe2528870a61be7eac5039982dde178f0f6c
wangshiz <wangshiz@users.noreply.github.com>
2018-09-13 22:07:07 +0800

    新增代码
    Signed-off-by: wangshiz <wangshiz@users.noreply.github.com>

---
+++ PythonGrammar/IfElse.py
@@ -0,0 +1,12 @@
+gender = input("你是男的还是女的？")
+if gender == '男':
+    print("不放你进来")
+elif gender == '女':
+    age = input("你多大了啊？")
+    if int(age) <= 40:
+        print("放你进来")
+    else:
+        print("不放")
+else:
+    print("打死都不放进来")
+

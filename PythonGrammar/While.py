27b93f6dbfa2c01f5370983cc377fd2f1100a5a3
Temmple <wangsz@qinqinhealth.com>
2018-09-14 14:19:30 +0800

    新增代码

--- PythonGrammar/While.py
+++ PythonGrammar/While.py
@@ -6,10 +6,30 @@ while True:
 
 a = 1
 sums = 0
-while a <= 100:
+while a <= 1031313:
     sums = sums + a
     a = a + 1
 
 print(sums)
 
+"""
+break & continue
+"""
+
+while True:
+    s = input("输入一段话:")
+    if s == 'q':
+        break   # 停止当前循环
+    if "马化腾" in s:
+        print("输入的话有关键字")
+        continue    # 停止本次循环开始下次循环
+    print("输入的一段话:"+s)
+
+# 1-100内所有的奇数
+
+a = 1
+while a <= 100:
+    if a % 2 == 1:
+        print(a)
+    a = a + 1
 

90634b608f349b6b655563b491c09f26596acfd2
wangshiz <wangshiz@users.noreply.github.com>
2018-09-27 22:33:12 +0800

    新增选择排序冒泡排序
    Signed-off-by: wangshiz <wangshiz@users.noreply.github.com>

--- Function/DynamicReference.py
+++ Function/DynamicReference.py
@@ -1,12 +1,23 @@
 """
 动态传参
 """
+
+'''
+位置参数：a，b
+动态参数：*xx
+默认参数：c=..
+关键字参数：**xx
+
+'''
+
+
 # 位置参数 > 动态参数 > 默认参数
 def eat(a, b, *food, c=5):
     print("我要吃", food)  # 动态接收的是元组数据
     print(a, b, food, c)
     print(globals())    # 查看全局作用域中所有名字
     print(locals())     # 查看当前作用域中所有名字
+
 
 eat(1, 2, 3, 4, 5, c=6)
 
@@ -17,4 +28,23 @@ eat(1, 2, 3, 4, 5, c=6)
 def func(**food):
     print(food)     # 返回一个字典
 
-func(a=1, b=2, c=3)
+
+func(a=1, b=2, c=3, d=4)
+
+
+# 位置，动态，默认值，关键字
+def fun2(a, b, *foods, c=3, **cars):
+    print(a, b, foods, c, cars)
+
+
+fun2(1, 2, 3, 4, 5, 6, c=3, d=4, e=5)
+
+
+def fun3(a, b):
+    print(a, b)
+
+
+# */** 运用在实参上，解包元组/字典形式结构
+fun3(*(1, 2))
+
+fun3(**{"a": 2, "b": 2})
--- Function/Global&Nonlocal.py
+++ Function/Global&Nonlocal.py
@@ -6,3 +6,17 @@ def function():
 
 function()
 print(a)
+
+# 20 20
+
+
+a = 10
+def fun2():
+    a = 20
+    def fun3():
+        nonlocal a  # 不取本地的参数  也不取全局参数
+        a = 2
+        print(a)
+    fun3()
+fun2()
+print(a)
